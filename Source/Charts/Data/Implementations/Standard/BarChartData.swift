d856cb450892794b5187acdbf55c58ec572b8fbe
Edgar <edgar5780@gmail.com>
2022-01-18 13:55:15 +0100

    Add round corner for BarChart

--- Source/Charts/Data/Implementations/Standard/BarChartData.swift
+++ Source/Charts/Data/Implementations/Standard/BarChartData.swift
@@ -11,6 +11,9 @@
 
 import Foundation
 import CoreGraphics
+#if !os(OSX)
+    import UIKit
+#endif
 
 open class BarChartData: BarLineScatterCandleBubbleChartData
 {
@@ -33,6 +36,13 @@ open class BarChartData: BarLineScatterCandleBubbleChartData
     ///
     /// **default**: 0.85
     @objc open var barWidth = Double(0.85)
+
+    #if !os(OSX)
+    /// Corners to apply the corner radius
+    ///
+    /// **default**: []
+    @objc open var cornersRadius: UIRectCorner = []
+    #endif
     
     /// Groups all BarDataSet objects this data object holds together by modifying the x-value of their entries.
     /// Previously set x-values of entries will be overwritten. Leaves space between bars and groups as specified by the parameters.
